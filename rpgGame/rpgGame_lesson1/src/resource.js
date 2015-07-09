220bb87b95010fe40531247f408d8858b3b68005
shenxing <52305556@163.com>
2015-07-09 18:07:54 +0800

    删除资源

--- rpgGame/rpgGame_lesson1/src/resource.js
+++ rpgGame/rpgGame_lesson1/src/resource.js
@@ -5,8 +5,6 @@ var res = {
     Hero_plist:"res/hero.plist",
     Tiger_png:"res/tiger.png",
     Tiger_plist:"res/tiger.plist",
-    Runner_png:"res/running.png",
-    Runner_plist:"res/running.plist",
     Map1_1_png : "res/400000.png",
     Map1_2_png : "res/400000_2.png",
     Map1_3_png : "res/400000_3.png"
